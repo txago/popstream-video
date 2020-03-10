ed6381c2c349935f2f0983424f7e475baac5e8a7
Thiago Menezes <thiagorsm@gmail.com>
2020-03-10 16:50:34 -0300

    adds config initialize fix

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+Rails.application.configure do
+  config.assets.initialize_on_precompile = false
+end
