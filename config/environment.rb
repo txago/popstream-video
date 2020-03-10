6e17a9ac5f7ffec9e1c7cfff110acb49d0980a66
Thiago Menezes <thiagorsm@gmail.com>
2020-03-10 17:09:37 -0300

    fix puma

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-Rails.application.configure do
-  config.assets.initialize_on_precompile = false
-end
