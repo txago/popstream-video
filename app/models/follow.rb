b979eba0104872a76980248e007c4650ac4b0758
Bruno Pellizzetti <pelli.br@gmail.com>
2020-03-11 11:42:14 -0300

    user follow system

---
+++ app/models/follow.rb
@@ -0,0 +1,5 @@
+class Follow < ApplicationRecord
+  # Follow mockup
+  belongs_to :follower, foreign_key: 'follower_id', class_name: 'User'
+  belongs_to :following, foreign_key: 'following_id', class_name: 'User'
+end
